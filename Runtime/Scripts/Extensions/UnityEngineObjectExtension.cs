4de9531ada658c1a067638688c3c9e6740d12303
nunun <hoge.life@gmail.com>
2022-04-30 23:23:40 +0900

    Implement packages.

---
+++ Runtime/Scripts/Extensions/UnityEngineObjectExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace GGLabo.Games.Runtime.Extensions
+{
+    public static class UnityEngineObjectExtension
+    {
+        /// <summary>
+        /// Cast null in UnityEngine.Object to null in System.Object.
+        /// Convenient to use null operators. (??, ?., ?[])
+        /// <a href="https://qiita.com/up-hash/items/91a4ea63ccfa7a1323c5">link</a>
+        /// </summary>
+        /// <param name="obj">Object to cast.</param>
+        /// <typeparam name="T">Type to cast.</typeparam>
+        /// <returns>Null in System.Object when obj is null.</returns>
+        public static T NullCast<T>(this T obj)
+            where T : UnityEngine.Object
+        {
+            return (obj != null) ? obj : null;
+        }
+    }
+}
---
+++ Tests/Runtime/Scripts/Extensions/UnityEngineObjectExtensionTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using GGLabo.Games.Runtime.Extensions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Object = UnityEngine.Object;
+
+namespace GGLabo.Games.Tests.Runtime.Extensions
+{
+    public class UnityEngineObjectExtensionTest
+    {
+        [UnityTest]
+        public IEnumerator NullCastTest()
+        {
+            var gameObject = new GameObject("Rigidbody");
+            var rigidbody = gameObject.AddComponent<Rigidbody>();
+
+            Object.DestroyImmediate(rigidbody);
+            yield return null;
+
+            Assert.That(rigidbody.NullCast()?.velocity.x ?? 100.0f, Is.EqualTo(100.0f));
+        }
+    }
+}
